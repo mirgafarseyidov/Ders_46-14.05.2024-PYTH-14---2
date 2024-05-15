abfaae9f5a47eef1ef38b76df4a655873013da48
mirgafarseyidov <161619722+mirgafarseyidov@users.noreply.github.com>
2024-05-15 20:53:14 +0400

    first commit

---
+++ Packets/samitler.py
@@ -0,0 +1,12 @@
+
+
+def finder(sentence):
+    consonants=['b','c','ç','ğ','ş','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+    temp=[]
+    for i in sentence:
+        if i.lower() in consonants and not i.lower() in temp:
+            temp.append(i.lower())
+    print(temp)
+
+
+          
---
+++ main.py
@@ -0,0 +1,25 @@
+
+"""
+Salman müəllim 
+
+Home Task  - 1 ci versiya 
+
+
+Bir Packets adinda folder(Paket) yaradin. Icinde samitler adinda bir file (modul) olsun.
+Onunda icinde samitleri_al adinda bir funksiya olsun.
+
+Nece calisacaq ?:
+Bu folderden colde yerlesen main.py faylimizda bu methodu cagirmali ve icine bir deyer (cumle) gondermeliyik.
+
+Numune cagrilma:
+'Salam necesen ... '
+
+netice:
+{'s', 'l', 'm', 'n', 'c'} >> heresinden bir dene olsa kifayetdir
+"""
+
+
+
+from Packets.samitler import finder
+
+finder("salam necesen")
